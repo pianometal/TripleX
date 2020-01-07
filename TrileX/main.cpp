559132525a09261e0e147e9b57d2d5605bc07e85
Kyle Lovely <kylesphere@gmail.com>
2020-01-07 12:04:52 -0500

    Initial Commit

---
+++ TrileX/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TrileX
+//
+//  Created by Kyle Lovely on 1/7/20.
+//  Copyright © 2020 Kyle Lovely. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
